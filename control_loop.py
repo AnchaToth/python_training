90df99be74dd0c1bb19b2cf2b9c46f20d174757c
AnchaToth <ancha.tot@gmail.com>
2022-03-02 16:18:24 +0100

    ido 16:18

---
+++ control_loop.py
@@ -0,0 +1,8 @@
+print("begin")
+
+for i in range(0,5):
+    print(i)
+    print("hello world")
+    print("hello python")
+print("exit")
+
---
+++ name_repeater.py
@@ -0,0 +1,15 @@
+name = input("Add meg a neved:")
+counter = int(input("Adj meg egy szamot: "))
+
+for i in range(0,counter):
+    print(i+1,name)
+
+print("half")
+
+for name in ["john", "jack", "jane"]:
+    print(name)
+
+for i in [2,4,6,8,10]:
+    print(i)
+
+print("End")
