f6df4bad7a39d3376f7af2bfc095b6f93385915e
Résztvevő - 13 <T360-v2-DTPB-s13@oktatas.local>
2022-03-02 12:16:18 +0100

    Initial commit

---
+++ variables.py
@@ -0,0 +1,29 @@
+print(3.13)
+
+employee_name = "John Doe"
+print(employee_name)
+
+age = 30
+print(age)
+age = 31
+print(age)
+
+print(employee_name)
+
+color_of_eyes = "blue"
+color_of_eyes_of_john = color_of_eyes
+
+print(color_of_eyes_of_john)
+
+print(type(3.4))
+print(type("soemthing"))
+
+age = "harminc"
+print(type(age))
+
+salary : int = 200
+print(salary)
+salary ="kétszáz"
+print(salary)
+print(type(salary))
+
