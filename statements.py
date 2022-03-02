b3f40cb1cb073927b739df36dd9d2d710961ba33
AnchaToth <ancha.tot@gmail.com>
2022-03-02 13:59:26 +0100

    testing string

---
+++ statements.py
@@ -0,0 +1,15 @@
+print(5*6)
+print(5+6)
+print(3/2)
+print(3-2)
+
+print(5*6+12)
+print(1+2*3)
+
+print("alma"+"korte")
+
+#print("alma" +3) error? CAN ONLY CONCAT " STRINGS
+
+print("alma"*3) #prints it 3 times
+#print("alma"-"a") not supported
+#print("alma"*"a") not supported, can only multiply a string with an integer
