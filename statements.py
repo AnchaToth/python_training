251105493c8ba4723612b3793c464b6ed39a75a6
AnchaToth <ancha.tot@gmail.com>
2022-03-02 15:01:24 +0100

    statement felido

--- statements.py
+++ statements.py
@@ -8,8 +8,26 @@ print(1+2*3)
 
 print("alma"+"korte")
 
-#print("alma" +3) error? CAN ONLY CONCAT " STRINGS
+#print("alma" +3) error? can only concat " strings
 
 print("alma"*3) #prints it 3 times
 #print("alma"-"a") not supported
 #print("alma"*"a") not supported, can only multiply a string with an integer
+
+name ="john doe"
+message ="a name valtozo tipusa: "+str(type(name)) #'class típust nem lehet konkatenálni ezért stringgé konvertáljuk
+
+
+print("az almak szama: "+str(5))
+
+print(len("hello"))
+
+hours = 3
+minutes =hours*60
+print(str(hours)+" óra = "+str(minutes)+" perc")
+
+word = "valami_nagyon_hosszu_karakterlanc"
+print("a hosszu szó = "+(str(len(word)))+" karakter")
+
+
+
