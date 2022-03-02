59517719505fc433e0fab9bfeab14f52a4273454
AnchaToth <ancha.tot@gmail.com>
2022-03-02 16:54:15 +0100

    ido 16:54

---
+++ months.py
@@ -0,0 +1,9 @@
+for month in ["january","february","march"]:
+    print(month)
+print("End")
+
+i=1
+for month in ["Január","Február","Március"]:
+    print("Az év "+str(i)+". hónapja: "+month)
+    i=i+1
+print("end")
