a9e8b9cc0d23fa176c8747b8ef0293d215efbd05
AnchaToth <ancha.tot@gmail.com>
2022-03-04 16:43:26 +0100

    3rd day @16:45

--- lists.py
+++ lists.py
@@ -21,3 +21,18 @@ employee = ["John Doe", 20]
 
 print(len(numbers))
 print("John Doe" in names)  # boolean-nel tér vissza
+
+## irj egy fv-t ami parameterul kap egy listat, amiben nevek vannak es visszaad egy masik listat
+## a masik listaban csak azok a nevek legyenek benne amik John-nal kezdodnek
+def pick_johns(names_list):
+    johns_list = []
+    for name in names_list:
+        # if "John" in name:
+        #      if name.index("John") == 0:
+        #         johns_list.append(name)
+        if "John" in name and name.index("John") == 0:
+            johns_list.append(name)
+    return johns_list
+
+names_list = ["John Doe", "Jack Doe", "John Smith", "Jane John"]
+print(pick_johns(names_list))
